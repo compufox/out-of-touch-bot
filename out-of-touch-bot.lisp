0992294fe2d46da03bb881c717c328c125a9d67a
ava fox <dev@computerfox.xyz>
2021-08-16 12:43:45 -0400

    added ability to post a second video

--- out-of-touch-bot.lisp
+++ out-of-touch-bot.lisp
@@ -2,17 +2,25 @@
 
 (in-package #:out-of-touch-bot)
 
+(defun choose (c1 c2)
+  (let ((chance (random 2)))
+    (if (zerop chance) c1 c2)))
+
 (defun main ()
-  (unless (uiop:file-exists-p "video.mp4")
-    (format t "wha- where's the out of touch thursdays video? ;w;")
-    (uiop:quit 1))
-  (handler-case
-      (with-user-abort
-          (run-bot ((make-instance 'mastodon-bot :config-file "config.file")
-                    :with-websocket nil)
-            (on (:thursday :at "11:00")
-              (post "its out of touch thursdays!" :visibility :public :media #P"video.mp4"))))
-    (user-abort ()
-      (uiop:quit))
-    (error (e)
-      (format t "error: ~a~%" e))))
+  (let ((video2 (uiop:file-exists-p "video2.mp4")))
+    (unless (uiop:file-exists-p "video.mp4")
+      (format t "wha- where's the out of touch thursdays video? ;w;")
+      (uiop:quit 1))
+    (handler-case
+        (with-user-abort
+            (run-bot ((make-instance 'mastodon-bot :config-file "config.file")
+                      :with-websocket nil)
+              (on (:thursday :at "11:00")
+                (post "its out of touch thursdays!" :visibility :public :media 
+                      (if video2 
+                          (choose #P"video.mp4" #P"video2.mp4")
+                          #P"video.mp4")))))
+      (user-abort ()
+        (uiop:quit))
+      (error (e)
+        (format t "error: ~a~%" e)))))
